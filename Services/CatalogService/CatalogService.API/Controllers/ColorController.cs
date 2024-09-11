df7483bebee74c94225e1516f0c01ce131c85975
Trần Thanh Nhân <nhancmvn12@gmail.com>
2024-09-11 23:00:23 +0700

    Refactor: reduce the excess parts

--- Services/CatalogService/CatalogService.API/Controllers/ColorController.cs
+++ Services/CatalogService/CatalogService.API/Controllers/ColorController.cs
@@ -1,5 +1,7 @@
 ﻿using CatalogService.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Globalization;
 
 namespace CatalogService.API.Controllers;
 
@@ -8,14 +10,18 @@ namespace CatalogService.API.Controllers;
 public class ColorController : ControllerBase
 {
     private readonly IColorService _service;
-    
-    public ColorController(IColorService service)
+    private readonly IDistributedCache _distributedCache;
+
+    public ColorController(IColorService service, IDistributedCache distributedCache)
     {
         _service = service;
+        _distributedCache = distributedCache;
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll() => Ok(await _service.GetAllAsync());
+    public async Task<IActionResult> GetAll() {
+        return Ok(await _service.GetAllAsync());
+    }
 
     // [HttpGet("{id}")]
     // public async Task<ColorDto?> Get(string id)
@@ -43,5 +49,25 @@ public class ColorController : ControllerBase
     // }
 
     [HttpDelete("{id}")]
-    public async Task<bool> Delete(Guid id) => await _service.DeleteAsync(id);
+    public async Task<bool> Delete(Guid id)
+    {
+        return await _service.DeleteAsync(id);
+    }
+
+    [HttpGet("RedisColor")]
+    public async Task<string> GetColorRedis()
+    {
+        var cacheKey = "The Time";
+        var currentTime = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+        var cachedTime = await _distributedCache.GetStringAsync(cacheKey);
+        if (string.IsNullOrEmpty(cachedTime))
+        {
+            var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(5));
+            await _distributedCache.SetStringAsync(cacheKey, currentTime, options);
+            cachedTime = await _distributedCache.GetStringAsync(cacheKey);
+        }
+
+        var result = $"Current Time: {currentTime} \nCached Time: {cachedTime}";
+        return result;
+    }
 }
--- Services/CatalogService/CatalogService.API/Program.cs
+++ Services/CatalogService/CatalogService.API/Program.cs
@@ -1,61 +1,19 @@
-#region Using
-
-using System.Globalization;
-using System.Reflection;
-using CatalogService.Common.Constants;
 using CatalogService.Application.Interfaces;
 using CatalogService.Domain.Entities;
 using CatalogService.Application.RabbitMQ;
 using CatalogService.Application.Repositories;
-using CatalogService.Common.Resources;
 using CatalogService.Application.Services;
-using Hangfire;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using CatalogService.Infrastructure;
 using CatalogService.Application;
-using Microsoft.AspNetCore.RateLimiting;
-using System.Threading.RateLimiting;
-
-#endregion
-
-#region Builder
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOptions();
-builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddRateLimiter(options =>
-{
-    options.AddFixedWindowLimiter("Fixed", options =>
-    {
-        options.PermitLimit = 10;
-        options.Window = TimeSpan.FromMinutes(1);
-        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-        options.QueueLimit = 20;
-    });
-});
-
-#region Database
-
-builder.Services.AddDbContext<StoreDbContext>(options => 
-                        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlDB")), ServiceLifetime.Scoped);
-builder.Services.AddHangfire(x =>
-{
-    x.UseSqlServerStorage(builder.Configuration.GetConnectionString("SqlDB"));
-});
-builder.Services.AddHangfireServer();
-
-//builder.Services.AddIdentity<User, IdentityRole>()
-//                .AddEntityFrameworkStores<StoreDbContext>()
-//                .AddDefaultTokenProviders();
-
-#endregion
+builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(
+                 builder.Configuration.GetConnectionString("SqlDB")), ServiceLifetime.Scoped);
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
@@ -64,71 +22,23 @@ builder.Services.AddStackExchangeRedisCache(options =>
 });
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
 
-builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", 
-    new OpenApiInfo { Title = "My API", Version = "v1" })
-);
-builder.Services.AddCors(options =>options.AddPolicy("AllowSpecificOrigin",
-                                                     b => b.WithOrigins("https://localhost:5077/")
-                                                           .AllowAnyHeader()
-                                                           .AllowAnyMethod()));
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IColorService, ColorService>();
 builder.Services.AddScoped<IRepository<Color>, ColorRepository>();
 builder.Services.AddScoped<IMessageProducer, RabbitMqProducer>();
 
-#endregion
-
-#region Localizations
-
-builder.Services.AddLocalization(options => options.ResourcesPath = ClothingConstants.Resource);
-builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
-{
-    options.DataAnnotationLocalizerProvider = (_, factory) =>
-    {
-        var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName!);
-        return factory.Create("ShareResource", assemblyName.Name!);
-    };
-});
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = new List<CultureInfo>
-    {
-        new CultureInfo("vi-VI"),
-        new CultureInfo("en-US")
-    };
-    options.DefaultRequestCulture = new RequestCulture(culture: "vi-VI", uiCulture: "vi-VI");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
-});
-
-#endregion
-
-#region App
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint(ClothingConstants.ApiSwaggerRoute, ClothingConstants.ApiSwaggerVersion);
-    });
+    app.UseSwaggerUI();
 }
 
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseRequestLocalization(app.Services
-    .GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.UseCors("AllowSpecificOrigin");
-app.UseHangfireDashboard();
-app.UseRateLimiter();
+app.UseHttpsRedirection();
+app.MapControllers();
+
 app.Run();
 
-#endregion
